88462c94663be7ea519693abc73c84bb4a37a8d9
shohret550 <shohret550@gmail.com>
2017-12-08 16:51:48 +0400

    Header almost complated excepted js part of responsive dropdawn menu

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -21,14 +21,19 @@ $(document).ready(function () {
 
 
 //Switching to native javascript
-//Add active class to the active page
+//Add active class to the active page`
 var pageTabs = document.getElementsByClassName("pageTabs");
 function activeTab(elem) {  
     for (var i = 0; i < pageTabs.length; i++) {
         if (elem != pageTabs[i]) {
             pageTabs[i].classList.remove("active");
         } else {
-            pageTabs[i].classList.add("active");
+            if (elem.classList.contains("child")) {
+                elem.parentElement.parentElement.previousElementSibling.classList.add("active");
+            }
+            else{
+                pageTabs[i].classList.add("active");
+            }
         }
     }
 }
