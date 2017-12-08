f662c4f307dd3c742de504bc84bc2f6a81ed4dde
shohret550 <shohret550@gmail.com>
2017-12-09 00:46:10 +0400

    Header complated

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -10,20 +10,53 @@ $(document).ready(function () {
         $(".search").hide("slow");
     });
 
+    //Show and hide navbar submenu
     $(".subMenuLi").hover(function () {
         $(this).find("ul").show(300);
     });
-    
+
     $(".subMenuLi").mouseleave(function () {
         $(".subMenu").hide("slow");
     });
+
+    //Show and hide responsive navbar menu
+    $(".navMenuSmallTitle .searchIconSmall i").click(function () {
+        if (this.classList.contains("fa-bars")) {
+            this.classList.remove("fa-bars");
+            this.classList.add("fa-times");
+        } else {
+            this.classList.remove("fa-times");
+            this.classList.add("fa-bars");
+        }
+        $(".menuResponse").toggle("slow");
+    });
+
+    //Show and hide responsive navbar submenus
+    $(".menuResponse i").click(function () {
+        var thisNext = $(this).next();
+        if (thisNext.hasClass("hiden")) {
+            this.classList.remove("fa-plus");
+            this.classList.add("fa-minus");
+            thisNext.show();
+            thisNext.removeClass("hiden");
+            thisNext.addClass("showen");
+        } else {
+            this.classList.remove("fa-minus");
+            this.classList.add("fa-plus");
+            thisNext.hide();
+            thisNext.removeClass("showen");
+            thisNext.addClass("hiden");
+        }
+        // $(".menuResponse").toggle("slow");
+    });
+
 });
 
 
 //Switching to native javascript
 //Add active class to the active page`
 var pageTabs = document.getElementsByClassName("pageTabs");
-function activeTab(elem) {  
+function activeTab(elem) {
     for (var i = 0; i < pageTabs.length; i++) {
         if (elem != pageTabs[i]) {
             pageTabs[i].classList.remove("active");
@@ -31,7 +64,7 @@ function activeTab(elem) {
             if (elem.classList.contains("child")) {
                 elem.parentElement.parentElement.previousElementSibling.classList.add("active");
             }
-            else{
+            else {
                 pageTabs[i].classList.add("active");
             }
         }
